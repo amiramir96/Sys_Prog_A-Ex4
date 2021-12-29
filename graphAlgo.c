54bd14bb76ca377380e77fdc5d5903dd4aaa0507
Ori Darshan <oridarshan@gmail.com>
2021-12-29 10:41:19 +0200

    dijkstra header and plan

---
+++ graphAlgo.c
@@ -0,0 +1,10 @@
+#include "graph.h"
+
+int shortest_path(pnode head, int src, int dest){
+    //create list for distances, fathers
+    //initiate distance to infinity for all edges, fathers to null
+    //src dist to 0, update nighbors distance and insert his nighbors to list
+    //  {run over list, take minimal distance node as current
+    //  update distance to nighbors, add them to list. set current to visited.}
+    //repeat until list is empty
+}
--- graphAlgo.h
+++ graphAlgo.h
@@ -1,1 +1,4 @@
+#include "graph.h"
+
 // Algorithms:
+int shortest_path(pnode head, int src, int dest);
