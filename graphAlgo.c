fe2c514435f5f955ecb9b8aa23eb541d39484f9a
Ori Darshan <oridarshan@gmail.com>
2021-12-29 20:00:30 +0200

    initiate nodes method

--- graphAlgo.c
+++ graphAlgo.c
@@ -1,4 +1,21 @@
 #include "graph.h"
+#include <math.h>
+
+/**
+ * @brief set all nodes distances to 'inf' except the src node which will be 0
+ * 
+ * @param head 
+ * @param src 
+ */
+void initiate_nodes(pnode head, int src){
+    pnode curr = head;
+    while(curr->next){
+        curr->dist = INFINITY; // dist is double
+        curr = head->next;
+    }
+    curr = get_node(&head, src);
+    curr -> dist = 0;
+}
 
 int shortest_path(pnode head, int src, int dest){
     //create list for distances, fathers
@@ -7,4 +24,8 @@ int shortest_path(pnode head, int src, int dest){
     //  {run over list, take minimal distance node as current
     //  update distance to nighbors, add them to list. set current to visited.}
     //repeat until list is empty
+
+    pnode curr_node = head;
+    initiate_nodes(head, src);
+    
 }
