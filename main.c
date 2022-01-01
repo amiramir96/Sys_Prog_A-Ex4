b30c26e45f6e14ceccd30736ea005072abb082b8
amiramir96 <89981387+amiramir96@users.noreply.github.com>
2022-01-01 21:45:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ int main(){
     pedge e, tempE;
     
     for(;;){
-        chr = 'q';
         scanf(" %c",&chr);
         
         if (chr == 'A'){ // case A create whole graph (linked list of nodes and ea node hold list of edges)
@@ -45,6 +44,8 @@ int main(){
                 }
                 idx++;
             }
+            chr = 'q';
+
         }
         else if (chr == 'B'){ // B case - add specific one node
             if (scanf(" %d ",&node_id)){ // else, skip.
@@ -61,16 +62,19 @@ int main(){
                     insert_edge(&head, n->node_id, weight, dest_node);
                 }
             }
+            chr = 'q';
         }
         else if (chr == 'D'){ // delete node
             scanf(" %d ",&node_id);
             delete_node_cmd(&head, node_id);
+            chr = 'q';
         }
         else if (chr == 'S'){ // shortest path
             int src, dest;
             scanf(" %d ",&src);
             scanf(" %d ",&dest);
             printf("Dijsktra shortest path: %d\n",shortest_path(head, src, dest));
+            chr = 'q';
         }
         else if (chr == 'T'){ // TSP - idont know when we ended to get inputs for TSP...
             int size = get_size(head);
@@ -91,6 +95,7 @@ int main(){
             printGraph_cmd(head);
             printf("TSP shortest path: %d\n",TSP(head, &arr[0], i));
             free(arr);
+            chr = 'q';
         }
         else {
             break;
